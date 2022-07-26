55938bacb41ee835a6611e56225a7eda7bdc6a5b
Fahd-O <96747618+Fahd-O@users.noreply.github.com>
2022-07-26 15:31:51 +0100

    Add files via upload

---
+++ Script..js
@@ -0,0 +1,70 @@
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+
+
+
+function _(x)
+{
+    return document.getElementById(x);
+}
+
+function renderQuestion()
+{
+    test = _("test");
+
+    if(pos >= questions.length)
+    {
+        test.innerHTML = "<h2>you got "+correct+" of "+questions.length+" questions correct</h2> <button id='button'> <a href='index.html'>Done</a> </buton>";
+
+        _("test_status").innerHTML = "Test Completed";
+
+        pos = 0;
+        correct = 0;
+
+        return false;
+    }
+
+    _("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+
+    question = questions[pos][0];
+    chA = questions[pos][1];
+    chB = questions[pos][2];
+    chC = questions[pos][3];
+    chD = questions[pos][4];
+
+    //puts question in its place
+    test.innerHTML = "<h3>"+question+"</h3>";
+
+    //puts options in their places
+    test.innerHTML += "<input type='radio' name='choices' value='A'>"+chA+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'>"+chB+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'>"+chC+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='D'>"+chD+"<br><br>";
+
+    //saves selected option & also used to proceed to next question
+    test.innerHTML += "<button onclick='checkAnswer()' id='button' >Next Question</button>";
+
+}
+
+function checkAnswer()
+{
+    choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++)
+    {
+        if(choices[i].checked)
+        {
+            choice = choices[i].value;
+        }
+    }
+    //if you want to let a user know if they got each answer correctly, this is where you put that work 
+    if(choice == questions[pos][5])
+    {
+        correct++;
+    }
+    
+    pos++;
+    renderQuestion();
+}
+
+window.addEventListener("load", renderQuestion, false);
+
+
---
+++ q_list..js
@@ -0,0 +1,64 @@
+//This file contains only question for the CBT or MCQ
+
+var questions = 
+[
+    ["1. A ________ is an electronic device that accepts data as input, stores data, processes this data based on user defined rules and gives out information as output", "Computer", "Refrigerator", "Mirror", "Living Thing", "A"],
+
+    ["2. The ________ components of a computer system are the data and the computer programs. ", "software", "Windows", "Light", "Linux", "A"],
+
+    ["3. When we opens a website on the internet, we see 'WWW.' in the Wedsite address. What is the full meaning of 'WWW' ?", "world wide woord", "world wide web", "word widing works", "world web wide", "B"],
+
+    ["<p style='text-align: center;'><img src='./Images./2_Cursor..png' alt='Cursor'></p> <br> 4. The arrow like symbol on the computer screen used for pointing, clicking, selecting etc... is called the  ?", "mouse", "arrow", "hand", "cursor", "D"],
+
+    ["5. Many of the interior hardware components of a computer are connected to the main circuit board, called the ___________", "Socket", "Motherboard", "White Board", "Fatherboard", "B"],
+
+    ["6. The ________ components of a computer system are the physical, electronic and mechanical parts we can see and touch.", "Application", "Hardware", "Code", "Software", "B"],
+
+    ["7. The ____________ supplies power for most of the components of a desktop computer system", "Solar Panel", "Food", "Power Supply", "Internet", "C"],
+
+    ["8. These are examples of Programming applications except ________", "A Website", "A Word Processing App", "A University Matriculation Program", "Road Traffic Light", "C"],
+
+    ["<img src='./Images./Keyboard..png' alt='Keyboard'> Keyboard <img src='./Images./Mouse..png' alt='Mouse'> Mouse <img src='./Images./Monitor..png' alt='Monitor'> Monitor <img src='./Images./Scanner..png' alt='Scanner'> Scanner <br><br><br> 9. These are examples of input devices, except ________", "The Keyboard right in front of you", "The mouse you're holding", "The screen you are looking at", "A Scanner", "C"],
+
+    ["10. Main memory is sometimes called ________ because it looses its information when power is removed.", "Volatile", "Super", "Controller", "Loser", "A"],
+
+    ["11. Main memory is where programs and data are kept when the processor is actively using them. When programs and data become active, they are copied from secondary memory into main memory where the processor can interact with them. A copy remains in secondary memory.", "True", "Disagree", "False", "Strongly Disagree", "A"],
+    
+    ["12. Main memory is sometimes called RAM. RAM stands for ___________", "Ram, Ram naw, you know, the normal Ram that we normally eat", "Rice And Meat", "Random Access Memory", "Read A Memory", "C"],
+    
+    ["13. Operating Systems can be accessed through two modes: ________ and _________", "Hardware and Software", "CLI and GUI", "Keyboard and mouse", "Computer and User", "B"],
+    
+    ["14. In Operating System terminology, CLI stands for __________", "Click", "Command Link", "Command Line Interface", "Computer Layer Input", "C"],
+    
+    ["15. Examples of output devices are these, except _________", "This screen you're looking at", "The speaker of this computer", "The built-in microphone of this PC", "All the printers at the admin block", "C"],
+    
+    ["16. In Operating System terminology, GUI stands for __________", "Graphical User Interface", "Great University of Ibadan (since it was invented there)", "Group User Internet", "Graph Unit Instance", "A"],
+    
+    ["17.  Secondary memory is usually called ___________ because it retains its information when power is removed.", "The reminder", "Non-volatile", "Bank", "Peripheral", "B"],
+    
+    ["18. A/An ________ is the most important software that runs on a computer. It manages the computer's memory and processes, as well as all of its software and hardware. It also allows you to communicate with the computer without knowing how to speak the computer's language. Without it, a computer is useless.", "Power Source", "Microsoft Word Application", "Operating System", "System Administrator", "C"],
+    
+    ["19. _________ created the Windows operating system in the mid-1980s", "Microsoft", "Baba ijebu", "Apple", "A Nigerian Man", "A"],
+    
+    ["20. In internet terminology, 'URL' stands for ________", "Uniform Research Linking", "Universal Resource Locator", "User Research Link", "UnRestricted Loading", "B"],
+    
+    ["21. It is a web-based tool or software program/system that is designed to search for information on the World Wide Web. What is it ?", "Website", "Browser", "Wordpress", "Search Engine", "D"],
+    
+    ["22. Mac OS is a line of operating systems created by _____", "Banana", "Mango", "Professor Ayomide Boluwatife", "Apple", "D"],
+    
+    ["23. Mobile devices such as phones, tablet computers, and MP3 players are different from desktop and laptop computers, so they run operating systems that are designed specifically for mobile devices. Which of the following is an example of a mobile Operating System", "Microsoft Windows OS", "Apple MacOS", "Google Android OS", "Linux Distributions", "C"],
+    
+    ["24. Which of this is an example of a popular programming language", "Mamba", "Python", "Cobra", "Codex", "B"],
+    
+    ["25.  Open-source operating systems can be modified and distributed by anyone around the world. This is different from proprietary softwares which can only be modified by the company that owns it. Which of the following is an example of an Open-source operating system:", "Open OS", "Apple OS", "Linux OS", "Windows OS", "C"],
+    
+    ["26. In web browsing Terminology, 'HTTP' stand for ________", "Hypertext Transfer Protocol", "Hit The Tab Page", "Highlight Text To Paste", "Highlight Total Page", "A"],
+    
+    ["27. A Graphical representation of an Algorithm is called _________", "Pseudocodes Chart", "Graphical Chart", "Problem Chart", "FlowChart", "D"],
+     
+    ["28. DBMS in computing terminology means _______", "Data Bank Micro Storage", "Database Management System", "Disc Base Mechanical Scanner", "Desktop Button Management System", "B"],
+    
+    ["29. 1 + 1 + 1 in Decimal base is ____", "1", "3", "111", "110", "B"],
+    
+    ["30. An Algorithm is defined as _______", "A set of problems to be solved by the computer", "A step by step procedure for solving a problem", "A mathematical formula for solving equations", "A set of codes used for unlocking a computer that the password has been forgotten", "B"]
+];
